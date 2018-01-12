c13a5f40212c54f711ae11f3bde6986b7c9dc6f6
Joel <c14jhn@cs.umu.se>
2018-01-12 02:30:21 +0100

    Project set up

---
+++ castle/src/main/java/castle/game/Main.java
@@ -0,0 +1,16 @@
+package castle.game;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+    public static void main( String[] args )
+    {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
---
+++ castle/src/test/java/castle/game/ModelTest.java
@@ -0,0 +1,4 @@
+package castle.game;
+
+public class ModelTest {
+}
